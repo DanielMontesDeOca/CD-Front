3996d88de7c2988fa59ff9991a0a64c5b8bcbfb2
DanielMontesDeOca <daniel@agavelab.com>
2016-01-08 11:47:46 -0600

    Simplified test for demo

--- tests/e2e/login.js
+++ tests/e2e/login.js
@@ -2,9 +2,6 @@ module.exports = {
   'Continuous delivery demo login test': function(browser) {
     browser
       .url('http://localhost:4000')
-      .waitForElementVisible('.login-form', 1000)
-      .assert.containsText('.login-form div:first-of-type a', 'Login')
-      .assert.containsText('.login-form div:last-of-type a', 'Register')
-      .end();
+      .expect.element('body').to.be.present;
   }
 };
